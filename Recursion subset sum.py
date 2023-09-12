bf879dc2787ac057757e53ce4be2411a8fc858a9
KiranEC11 <123507880+KiranEC11@users.noreply.github.com>
2023-09-13 02:50:12 +0530

    Create Recursion subset sum.py

---
+++ Recursion subset sum.py
@@ -0,0 +1,43 @@
+"""
+subset sum
+https://practice.geeksforgeeks.org/problems/subset-sums2234/1
+
+"""
+
+
+#User function Template for python3
+class Solution:
+	def subsetSums(self, arr, N):
+		# code here
+		
+		ans = []
+		def sumHelper(index,sum):
+		    if index==N:
+		        ans.append(sum)
+		    else:
+		        sumHelper(index+1,sum + arr[index])
+		        
+		        sumHelper(index+1, sum)
+	    sumHelper(0,0)
+	    ans.sort()
+	    return ans
+
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        N = int(input())
+        arr = [int(x) for x in input().split()]
+        ob = Solution()
+        ans = ob.subsetSums(arr, N)
+        ans.sort()
+        for x in ans:
+            print(x,end=" ")
+        print("")
+
+# } Driver Code Ends
